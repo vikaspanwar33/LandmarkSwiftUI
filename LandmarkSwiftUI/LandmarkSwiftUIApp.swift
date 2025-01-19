cd5189092af9bcfc1dadfa7343014473f0cdfd9a
Vikas <vikaspanwar971@gmail.com>
2025-01-19 12:02:06 -0500

    Initial Commit

---
+++ LandmarkSwiftUI/LandmarkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkSwiftUIApp.swift
+//  LandmarkSwiftUI
+//
+//  Created by Vikas on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
